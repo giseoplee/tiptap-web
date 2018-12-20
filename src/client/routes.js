58c3a16783ba66d2caa832ce648d7c8195fc5907
giseoplee <llgs901@naver.com>
2018-12-20 12:12:09 +0900

    (2018-12-20) update by giseop.lee : add modal & confirm

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import Loadable from 'react-loadable'
-import Loader from 'react-loader-spinner'
 
 import Layout from './containers/Layout';
 
 function Loading() {
-  return <Loader type="Rings" color="#dc3545" height="100"	width="100" className="custom-center"/>;
+  return <p className="page-spinner">
+          <i className="fa fa-spinner fa-spin custom-font-3rem mt-4 mb-1"></i>
+        </p>;
 }
 
 const BlameBoard = Loadable({
   loader: () => import('./views/Contents/Blame'),
-  loading: Loading,
+  loading: Loading
 });
 
 const BlockBoard = Loadable({
   loader: () => import('./views/Contents/Block'),
-  loading: Loading,
+  loading: Loading
 });
 
 const Login = Loadable({
   loader: () => import('./views/Pages/Login'),
-  loading: Loading,
+  loading: Loading
 });
 
 const routes = [
   { path: '/', exact: true, name: 'Home', component: Layout },
   { path: '/login', name: 'Login', component: Login },
-  { path: '/blame/list', name: 'Blame', component: BlameBoard },
-  { path: '/block/list', name: 'Block', component: BlockBoard }
+  { path: '/blame/list', name: '신고 사용자 관리', component: BlameBoard },
+  { path: '/block/list', name: '차단 사용자 관리', component: BlockBoard }
 ];
 
 export default routes;
